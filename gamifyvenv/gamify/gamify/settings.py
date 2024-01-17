41fb0b347367e07ed3b9dd6e6a8ff05e21a278ea
georgeaspr <ice19390024@uniwa.gr>
2024-01-17 17:09:04 +0200

    ImageUpdate 1.0
    (εικονες στα παιχνιδια, μη τελειωμενο)

--- gamifyvenv/gamify/gamify/settings.py
+++ gamifyvenv/gamify/gamify/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -124,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL= '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
