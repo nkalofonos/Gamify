6d5caed231cf563833e354ac9cc960c9df547a5e
Nkal01 <ice19390074@uniwa.gr>
2024-02-19 19:07:42 +0200

    Cosmetic changes

--- gamifyvenv/gamify/gamify/settings.py
+++ gamifyvenv/gamify/gamify/settings.py
@@ -136,10 +136,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL= '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print(STATIC_URL)
+print(BASE_DIR)
