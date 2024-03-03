17d96f502499c34366c0042fbe2c94413f1df6a8
MadLemonH <152202755+MadLemonH@users.noreply.github.com>
2024-03-03 16:51:57 +0200

    myaccount
    - Change password functionality added
    - styling

--- gamifyvenv/gamify/apps/userprofile/views.py
+++ gamifyvenv/gamify/apps/userprofile/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from apps.order.models import LibraryItem
 from apps.store.models import Product
-
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash 
+from django.contrib import messages  
 from .forms import SignUpForm
 
 def signup(request):
@@ -24,7 +26,21 @@ def signup(request):
 
 @login_required
 def myaccount(request):
-    return render(request, 'myaccount.html')
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('/')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+            form = PasswordChangeForm(request.user)
+    return render(request, 'myaccount.html', {
+            'form': form
+    })
+    
 
 def library(request, game_title=None):
     if request.user.is_authenticated:
